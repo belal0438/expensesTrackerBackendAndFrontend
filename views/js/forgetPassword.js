9d6b3fb591b132134ea6682a14a6506c48847e8d
Belal <mohammadvns121@gmail.com>
2023-05-29 12:37:35 +0530

    A user forgot his password.Frontend Deliverables

---
+++ views/js/forgetPassword.js
@@ -0,0 +1,21 @@
+
+
+const my_form = document.getElementById('my_form');
+
+my_form.addEventListener('submit', onsubmit);
+
+async function onsubmit(eve) {
+    try {
+        eve.preventDefault();
+        const EmailId = document.getElementById('useremail');
+
+        let obj = { emailId: EmailId.value };
+
+        let UseremaiIdPost = await axios.post("http://localhost:3000/password/forgotpassword", obj);
+        
+        console.log(UseremaiIdPost);
+
+    } catch (err) {
+        console.log(err)
+    }
+}
